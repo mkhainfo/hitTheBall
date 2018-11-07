52dd0bb230cb0a8b5a11c53be24430bf1380927f
mkhainfo <to@mkha.info>
2018-11-07 12:30:34 -0800

    ball movement initiated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//reference to inner window dimentions
 function getWindow() {
   var get = {
     w: window.innerWidth,
@@ -6,12 +7,16 @@ function getWindow() {
  return get
 }
 
+//a function to set multiple attributes at once
+//takes an element and an object
 function setAtt(el, att) {
   for(var key in att) {
     el.setAttribute(key, att[key])
   }
 }
 
+//set functions scale element to the inner window dimentions
+//using setAtt() to change attributes
 function setBound() {
   var get = getWindow()
   var bound = document.getElementById("bound")
@@ -50,25 +55,43 @@ function setBall() {
     cy: get.h / 2,
     r: dist / 100,
     fill: '#eeeeee',
+    dx: 1,
+    dy: 1,
   }
   setAtt(ball, ballAtt)
 }
 
+//defines the behavior of the ball
 function moveBall() {
-  var cell = document.getElementById("cell")
-  var ball = document.getElementById("ball")
-  var x = parseInt(ball.getAttribute("cx"), 10)
-  var y = parseInt(ball.getAttribute("cy"), 10)
-  var ballAtt = {}
-  //you are here~~ create bounce conditions for ball + cell
-  var rake = false
-  if (rake) {
-      console.log("rake")
-  } else {
-    ballAtt.cx = x + 1
-    ballAtt.cy = y + 1
+  var cell = {
+    cell: document.getElementById("cell"),
+    x: (() => parseInt(this.cell.getAttribute("x"), 10))(),
+    y: (() => parseInt(this.cell.getAttribute("y"), 10))(),
+    w: (() => parseInt(this.cell.getAttribute("width"), 10))(),
+    h: (() => parseInt(this.cell.getAttribute("height"), 10))(),
   }
-  setAtt(ball, ballAtt)
+  var ball = {
+    ball: document.getElementById("ball"),
+    x: (() => parseInt(this.ball.getAttribute("cx"), 10))(),
+    y: (() => parseInt(this.ball.getAttribute("cy"), 10))(),
+    dx: (() => parseInt(this.ball.getAttribute("dx"), 10))(),
+    dy: (() => parseInt(this.ball.getAttribute("dy"), 10))(),
+  }
+
+  if (ball.x + ball.dx < cell.x || ball.x + ball.dx > cell.x + cell.w) {
+    ball.dx *= -1
+  }
+  else if (ball.y + ball.dy < cell.y || ball.y + ball.dy > cell.y + cell.h) {
+      ball.dy *= -1
+  }
+
+  var ballAtt = {
+    cx: ball.x + ball.dx,
+    cy: ball.y + ball.dy,
+    dx: ball.dx,
+    dy: ball.dy,
+  }
+  setAtt(ball.ball, ballAtt)
 }
 
 function set() {
