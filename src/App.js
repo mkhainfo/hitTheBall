49e02e67547b26769e81103b5c28f50929868eff
mkhainfo <to@mkha.info>
2018-11-30 22:59:30 -0800

    score available, working on reset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,200 @@
 import React, { Component } from 'react'
-//import update from 'immutability-helper'
-import Game from './game/Game.js'
+import update from 'immutability-helper'
+//import Game from './game/Game.js'
 import './App.css'
+
 
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      score: 0,
+    }
   }
+
+getScore = (n) => {
+  this.setState({ score : n })
+}
 
   render() {
     return (
       <span id="view">
-        <Score style={styles.score} />
-        <div id="wrap" style={styles.game}>
-          <Game fill='#eeeeee' />
-        </div>
+        <Game fill='#eeeeee' score={this.getScore} />
+        <Score score={this.state.score} />
       </span>
+    )
+  }
+}
+
+class Game extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      svg: {},
+      cell: {},
+      paddle: {},
+      ball: {},
+      score: 0,
+    }
+  }
+
+  setSvg = () => {
+    let w = window.innerWidth, h = window.innerHeight
+    let svg = update(this.state.svg, {
+      w: {$set: w},
+      h: {$set: h},
+      box: {$set: [0, 0, w, h].join(' ')},
+      size: {$set: Math.sqrt( w*w + h*h )}
+    })
+    this.setState({ svg: svg })
+  }
+
+  setCell = () => {
+    let w = this.state.svg.w * .8, h = this.state.svg.h * .8
+    let cell = update(this.state.cell, {
+      w: {$set: w},
+      h: {$set: h},
+      x: {$set: (this.state.svg.w - w) / 2},
+      y: {$set: (this.state.svg.h - h) / 2},
+    })
+    this.setState({cell: cell})
+  }
+
+  setPaddle = () => {
+    let w = this.state.svg.w / 9, h = this.state.svg.h / 60
+    let cell = this.state.cell
+    let paddle = update(this.state.paddle, {
+      w: {$set: w},
+      h: {$set: h},
+      x: {$set: (this.state.svg.w - w) / 2},
+      y: {$set: cell.h + (this.state.svg.h - cell.h) / 2 - h},
+    })
+    this.setState({paddle: paddle})
+  }
+
+  setBall = () => {
+    let svg = this.state.svg
+    let ball = update(this.state.ball, {
+      x: {$set: svg.w/2},
+      y: {$set: svg.h/2},
+      r: {$set: svg.size/100},
+      dx: {$set: svg.size/700},
+      dy: {$set: svg.size/700},
+      })
+
+    this.setState({ ball: ball })
+  }
+
+  moveBall = () => {
+    let cell = this.state.cell, ball = this.state.ball,
+        pad = this.state.paddle, bounce = ball
+
+    if ( ball.x + ball.dx - ball.r < cell.x
+          || ball.x + ball.dx + ball.r > cell.x + cell.w) {
+      bounce = update(ball, {
+        dx: {$set: ball.dx * -1}
+        })
+    } else if (ball.y + ball.dy - ball.r  < cell.y) {
+      bounce = update(ball, {
+        dy: {$set: ball.dy * -1}
+        })
+    } else if ( ball.y + ball.r + ball.dy  > cell.y + cell.h - pad.h) {
+      let hit = ball.x + ball.r  < pad.x
+          || ball.x - ball.r > pad.x + pad.w ? 0 : -1
+      bounce = update(ball, {
+        dy: {$set: ball.dy * hit}
+        })
+      let score = hit ? this.state.score + 1 : ':('
+      this.setState({ score : score })
+      this.props.score(score)
+    }
+
+
+    let move = update(bounce, {
+      x: {$set: ball.x + ball.dx},
+      y: {$set: ball.y + ball.dy},
+      })
+
+    this.setState({ ball: move })
+  }
+
+  movePaddle = (e) => {
+    let move
+    let x = e.type !== 'mousemove'? e.touches.clientX : e.clientX
+    let pad = this.state.paddle.w / 2
+    let margin = (this.state.svg.w - this.state.cell.w) / 2 + pad
+    if (x < margin) {
+      move = update(this.state.paddle, {
+        x: {$set: margin - pad},
+        })
+    } else if (x > this.state.svg.w - margin) {
+      move = update(this.state.paddle, {
+        x: {$set: this.state.svg.w - margin - pad },
+        })
+    } else {
+      move = update(this.state.paddle, {
+        x: {$set: x - pad },
+        })
+    }
+    this.setState({ paddle: move })
+  }
+
+  setGame = () => {
+    this.setSvg()
+    setTimeout(this.setCell)
+    setTimeout(this.setBall)
+    setTimeout(this.setPaddle, 1)
+  }
+
+//// this conditions should be triggered AFTER state is updated to score: ':('
+//////     where do i put it?
+/*    if (this.state.score === ':(') {
+      let reset = () => {
+        this.setGame()
+        this.setState({ score : 0 })
+        this.props.score(0)
+      }
+      window.setTimeout(reset(), 3000)
+    }
+  }   */
+//////////   maybe in shouldComponentUpdate()? check the lifecycle graph
+///// maybe it is triggered when the ball touches the paddle again ??
+/// in that case it would be a part of moveBall()
+
+  componentDidMount() {
+    this.setGame()
+    window.addEventListener('resize', this.setGame)
+    window.setInterval(this.moveBall, 10)
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    return this.state !== nextState
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.setGame)
+    window.clearInterval(window.setInterval(this.moveBall, 10))
+  }
+
+  render() {
+    let s = this.state
+    return(
+      <svg
+        width={ s.svg.w } height={ s.svg.h }
+        viewBox={ s.svg.box }
+        onMouseMove={this.movePaddle} onTouchMove={this.movePaddle} >
+        <rect id='cell'
+          width={ s.cell.w } height={ s.cell.h }
+          x={ s.cell.x } y={ s.cell.y } />
+        <circle id='ball'
+          cx={ s.ball.x } cy={ s.ball.y }
+          r={ s.ball.r } fill={ this.props.fill } />
+        <rect id='paddle'
+          width={ s.paddle.w } height={ s.paddle.h }
+          x={ s.paddle.x } y={ s.paddle.y }
+          fill={ this.props.fill } />
+      </svg>
     )
   }
 }
@@ -32,19 +209,20 @@ class Score extends Component {
 
   render() {
     return (
-      <div id='score'><p>{this.state.score}</p></div>
+      <div id='score' style={styles.score}>
+        {this.props.score}
+      </div>
     )
   }
 }
 
 const styles = {
   score: {
-    color: '#ffffff',
+    fontSize: '6vh',
+    color: '#eee',
     textAlign: 'center',
-    zIndex: 2,
-  },
-  game: {
-    zIndex: 1,
+    zIndex: 3,
+    position: 'absolute',
   },
 }
 export default App
