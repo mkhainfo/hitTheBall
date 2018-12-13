d1717213aab7722eb4483d72b4fdb2c25cdeac39
mkhainfo <to@mkha.info>
2018-12-12 16:08:12 -0800

    Updates

---
+++ precache-manifest.e3c0e9a6e5a279d12899540abfd894fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "feecdac5ba95e6fb1fd4",
+    "url": "/hitTheBall/static/js/runtime~main.feecdac5.js"
+  },
+  {
+    "revision": "d5479f039852eaa59623",
+    "url": "/hitTheBall/static/js/main.d5479f03.chunk.js"
+  },
+  {
+    "revision": "3424e71d5ef9ddbb12d9",
+    "url": "/hitTheBall/static/js/1.3424e71d.chunk.js"
+  },
+  {
+    "revision": "d5479f039852eaa59623",
+    "url": "/hitTheBall/static/css/main.050d6860.chunk.css"
+  },
+  {
+    "revision": "c5eec073ae14c8ec791273b29a01880f",
+    "url": "/hitTheBall/index.html"
+  }
+];
